c71db34a99805e8001e21ace7b71ebf1c9f87c3d
jmfrey <jfrey@clarku.edu>
2019-10-30 18:47:32 -0400

    numpy prefix

--- Python/final_proj.py
+++ Python/final_proj.py
@@ -19,81 +19,81 @@ with open(my_path) as f:
     ncols = len(f.readline().split(',')) #comma delimited
 
 # load in csv, skipping first row and column
-contingency_table = np.loadtxt(open(my_path, "rb"), 
+contingency_table_np = np.loadtxt(open(my_path, "rb"), 
                                delimiter = ",", 
                                skiprows = 1,
                                usecols = range(1, ncols))
-print(contingency_table)
+print(contingency_table_np)
 
 
 #find sum of hits:
-total_hits = 0
+total_hits_np = 0
 for i in range(0, ncols-1):
-    total_hits = total_hits + contingency_table[i,i]
-
+    total_hits_np = total_hits_np + contingency_table_np[i,i]
     
 #find column sums
-column_totals = np.array([])
+column_totals_np = np.array([])
 for i in range(0, ncols-1):
-    column_totals = np.append(column_totals, sum(contingency_table[:,i]))
+    column_totals_np = np.append(column_totals_np, sum(contingency_table_np[:,i]))
 
 # find row totals:
-row_totals = np.array([])
+row_totals_np = np.array([])
 for i in range(0, ncols-1):
-    row_totals = np.append(row_totals, sum(contingency_table[i,:]))
+    row_totals_np = np.append(row_totals_np, sum(contingency_table_np[i,:]))
 
 
 # transpose of the matrix:
-contingency_table_transpose = contingency_table.transpose()
+contingency_table_transpose_np = contingency_table_np.transpose()
 
 # exchange for each category (remember that it comes in pairs (Dek):
-size_of_exchange_difference = np.zeros(shape=(1, ncols-1))
+size_of_exchange_difference_np = np.zeros(shape=(1, ncols-1))
 for i in range(0, ncols-1):
     current_exchange_category = np.zeros(shape=(1, ncols-1))
     for j in range(0, ncols-1):
-        current_exchange_category[0,j] = np.min((contingency_table[i,:][j], contingency_table[:,i][j]))
+        current_exchange_category[0,j] = np.min((contingency_table_np[i,:][j], contingency_table_np[:,i][j]))
         if j == ncols-2:
-            size_of_exchange_difference[0,i] = np.sum(current_exchange_category)-contingency_table[i,i]
-size_of_exchange_difference = size_of_exchange_difference * 2
+            size_of_exchange_difference_np[0,i] = np.sum(current_exchange_category)-contingency_table_np[i,i]
+size_of_exchange_difference_np = size_of_exchange_difference_np * 2
 
 
 # false alarms for k:
-all_false_alarms = np.zeros(shape=(1, ncols-1))
+all_false_alarms_np = np.zeros(shape=(1, ncols-1))
 for i in range(0, ncols-1):
-    all_false_alarms[0,i] =  sum(contingency_table[i,:]) - contingency_table[i,i]
+    all_false_alarms_np[0,i] =  sum(contingency_table_np[i,:]) - contingency_table_np[i,i]
     
 # misses for k:
-all_misses = np.zeros(shape=(1, ncols-1))
+all_misses_np = np.zeros(shape=(1, ncols-1))
 for i in range(0, ncols-1):
-    all_misses[0,i] = sum(contingency_table[:,i]) - contingency_table[i,i]
+    all_misses_np[0,i] = sum(contingency_table_np[:,i]) - contingency_table_np[i,i]
 
 
 # Quantity difference/disagreement for k (Dqk):
-size_of_quantity_difference = abs(all_misses - all_false_alarms)
+size_of_quantity_difference_np = abs(all_misses_np - all_false_alarms_np)
 
 # size of difference (false alarms + misses) for category k:
-size_of_difference = all_false_alarms + all_misses
+size_of_difference_np = all_false_alarms_np + all_misses_np
 
 # size of shift difference for k (Dsk):
-size_of_shift_difference = size_of_difference - size_of_quantity_difference - size_of_exchange_difference
+size_of_shift_difference_np = size_of_difference_np - size_of_quantity_difference_np - size_of_exchange_difference_np
 
 # sum of quantity, exchange, and shift
-qes_sum = size_of_quantity_difference + size_of_exchange_difference + size_of_shift_difference
+qes_sum_np = size_of_quantity_difference_np + size_of_exchange_difference_np + size_of_shift_difference_np
 
 # false alarm quantity
-false_alarm_quantity = np.zeros(shape = (1, ncols-1))
+false_alarm_quantity_np = np.zeros(shape = (1, ncols-1))
 for i in range(0, ncols-1):
-    false_alarm_quantity[0,i] = np.max((0, (all_false_alarms[0,i] - all_misses[0, i])))
+    false_alarm_quantity_np[0,i] = np.max((0, (all_false_alarms_np[0,i] - all_misses_np[0, i])))
 
 # miss quantity
-miss_quantity = np.zeros(shape = (1, ncols-1))
+miss_quantity_np = np.zeros(shape = (1, ncols-1))
 for i in range(0, ncols-1):
-    miss_quantity[0,i] = np.max((0, (all_misses[0,i] - all_false_alarms[0, i])))
+    miss_quantity_np[0,i] = np.max((0, (all_misses_np[0,i] - all_false_alarms_np[0, i])))
 
 
 
 #create Size dataframe:
-categories = np.array([range(0, ncols-1)])
+categories_np = np.array([range(0, ncols-1)])
+
 
 
 
