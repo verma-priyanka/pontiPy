0050455230fdd9dc94cd39061cb40b5a890eb3c3
jmfrey <jfrey@clarku.edu>
2019-11-07 22:05:41 -0500

    updates

--- Python/final_proj.py
+++ Python/final_proj.py
@@ -37,24 +37,31 @@ def extent(data):
     return(np.sum(data))
 
 # misses (binary or k):
-def misses(data):
+def misses(data, par1=None):
     if len(data) > 2:
         all_misses_np = np.zeros(shape=(1, len(data)))
         for i in range(0, len(data)):
             all_misses_np[0,i] = sum(data[:,i]) - data[i,i]
-        return(all_misses_np)
+        if type(par1) == type(None):
+            return(all_misses_np)
+        else:
+            return(all_misses_np)[0, par1]
     else:
         return(data[1,0])
 
 # hits (binary or k):
-def hits(data):
+def hits(data, par1=None):
     if len(data) > 2:
         all_hits_np = np.zeros(shape=(1, len(data)))
         for i in range(0, len(data)):
             all_hits_np[0,i] = data[i,i]
-        return(all_hits_np)
+        if type(par1) == type(None):
+            return(all_hits_np)
+        else:
+            return(all_hits_np[0, par1])
     else:
         return(data[0,0])
+        
 
 #correct rejections
 def correct_rejections(data):
@@ -64,12 +71,15 @@ def correct_rejections(data):
         return(data[1,1])
         
 # false alarms for k:
-def false_alarms(data):
+def false_alarms(data, par1=None):
     if len(data) > 2:
         all_false_alarms_np = np.zeros(shape=(1, len(data)))
         for i in range(0, len(data)):
             all_false_alarms_np[0,i] =  sum(data[i,:]) - data[i,i]
-        return(all_false_alarms_np)
+        if type(par1) == type(None):
+            return(all_false_alarms_np)
+        else:
+            return(all_false_alarms_np[0, par1])
     else:
         return(data[0,1])
     
@@ -87,9 +97,12 @@ def specificity(data):
     else:
         correct_rejections(data) / (correct_rejections(data) + false_alarms(data))
 
-
+# convert sample to population
 def sample_to_population(data, transformation = 1):
-    if np.shape(transformation) == (len(data), 1) or type(transformation) == int:
+    if (type(transformation) == np.ndarray and np.shape(transformation) == (len(data), 1)) or type(transformation) == int:
+        return(data * transformation)
+    elif type(transformation) == list:
+        transformation = np.vstack(np.array(transformation))
         return(data * transformation)
     else:
         print("The tranformation array provided has incorrect dimensions.")
@@ -135,6 +148,11 @@ def size_of_exchange_difference(data):
 def size_of_quantity_difference(data):
     size_of_quantity_difference_np = abs(misses(data) - false_alarms(data))
     return(size_of_quantity_difference_np)
+
+#######################
+#def is_quantity(data, par1=None):
+#    bool_list = np.ndarray.tolist(size_of_quantity_difference(data) > 0)[0]
+#    return([i for i, x in enumerate(bool_list) if x])[par1]
 
 # size of difference (false alarms + misses) for category k (total disagreement):
 def size_of_difference(data):
@@ -186,10 +204,9 @@ def miss_exchange(data):
     return(exchange)
 
 
-
-
-
-# false alarm exchange
+# miss shift
+    
+# false alarm shift
         
 
 
